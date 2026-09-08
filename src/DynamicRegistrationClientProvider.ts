f20d4fc08c6f085cec1ff88a3c030e37a48e44a1
Samu Lang <langsamu@hotmail.com>
2026-09-08 18:15:00 +0100

    Request refresh token in dyn client reg

--- src/DynamicRegistrationClientProvider.ts
+++ src/DynamicRegistrationClientProvider.ts
@@ -1,10 +1,16 @@
 import type { ClientProvider } from "./ClientProvider.js"
 import * as oauth from "oauth4webapi"
+import { supportsOfflineAccess } from "./supportsOfflineAccess.js"
 
 export class DynamicRegistrationClientProvider implements ClientProvider {
     async getClient(as: oauth.AuthorizationServer, redirectUri: string, signal: AbortSignal): Promise<oauth.Client> {
         const metadata = {
             redirect_uris: [redirectUri],
+            grant_types: ["authorization_code"]
+        }
+
+        if (supportsOfflineAccess(as)) {
+            metadata.grant_types.push("refresh_token")
         }
 
         const registrationResponse = await oauth.dynamicClientRegistrationRequest(as, metadata, {signal})
